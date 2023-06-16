3e2abb7a0141904036e8bc2c01b99d96099cb09e
Aaron <whizzyrecx@gmail.com>
2023-06-16 16:42:48 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Return 1
+ */
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+	return (1);
+}
