31679425c1b4d03564f76b74f19cd8106de01690
Aaron <whizzyrecx@gmail.com>
2023-06-16 14:51:49 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * function  main - Entry point
+ * puts - print to standard output
+ * reurns Always 0 (sucess)
+*/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	reurn(0)
+}
