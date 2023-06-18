cfdeddd3dd614e93a1000d20c377fd0e7510fda1
Aaron <whizzyrecx@gmail.com>
2023-06-18 01:19:16 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * prints all the numbers less than 5
+ *
+ * Return: Return 0
+ */
+int main(void)
+{
+	int num;
+
+	num = 0;
+	while (num <= 5)
+	{
+		printf("%d", num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
