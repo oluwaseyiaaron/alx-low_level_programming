07833a6f8014f9255467eb16230d59b7a8698578
Aaron <whizzyrecx@gmail.com>
2023-06-18 20:18:11 -0700

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int a, b;
 
-	for (a = 0; a < 10; a++)
+	for (a = 0; a < 9; a++)
 	{
 		for (b = 1; b < 10; b++)
 		{
@@ -18,7 +18,8 @@ int main(void)
 			{
 				putchar('0' + a);
 				putchar('0' + b);
-				if (a != 9 || b != 9)
+				if (a == 8 || b == 10)
+					continue;
 				{
 					putchar(',');
 					putchar(' ');
