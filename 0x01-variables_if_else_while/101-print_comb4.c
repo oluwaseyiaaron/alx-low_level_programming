4c460f3d40e922e679acdace2a92f37dc13ae486
Aaron <whizzyrecx@gmail.com>
2023-06-19 18:19:54 -0700

    101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,9 +12,9 @@ int main(void)
 
 	for (a = 0; a < 8; a++)
 	{
-		for (b = 1; b < 9; b++)
+		for (b = a + 1; b < 9; b++)
 		{
-			for (c = 2; c < 10; c++)
+			for (c = b + 1; c < 10; c++)
 			{
 				putchar((a % 10) + '0');
 				putchar((b % 10) + '0');
