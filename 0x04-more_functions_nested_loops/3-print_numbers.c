cc788d35fdc11be0ade5b2f0a252a0b00574334d
Aaron <whizzyrecx@gmail.com>
2023-06-23 16:17:59 -0700

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - prints number 0-9
+ * Return: return 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= 9)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
