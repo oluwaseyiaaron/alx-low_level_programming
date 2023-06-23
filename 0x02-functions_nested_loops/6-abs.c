ad46f618e2f202fa6930a56a6002782e54ee8dd4
Aaron <whizzyrecx@gmail.com>
2023-06-22 20:51:46 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - function that completes the absolute value of
+ *       an integer
+ *
+ * Return: always 0
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (n);
+}
