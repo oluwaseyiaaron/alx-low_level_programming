32632ae6bb474f403be196cc3b9b562a69777030
Aaron <whizzyrecx@gmail.com>
2023-06-22 22:07:33 -0700

    4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,11 +2,11 @@
 /**
  * _isalpha - checks if its a lowercase or uppercase
  *
+ * @c: takes input from other functions.
+ * 
  * Return: return 1 if true or else 0
  */
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122 && c >= 65 && c >= 90)
-		return (1);
-	return(0);
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
