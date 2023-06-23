a3ebd2298cb97034cc24e15381c21b463753140a
Aaron <whizzyrecx@gmail.com>
2023-06-23 15:49:49 -0700

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,81 @@
+#nclude "main.h"
+/**
+ * mul - multiply two numbers
+ * @a: first digit
+ * @b: second digit
+ * Return: always 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}#nclude "main.h"
+/**
+ * mul - multiply two numbers
+ * @a: first digit
+ * @b: second digit
+ * Return: always 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}#nclude "main.h"
+/**
+ * mul - multiply two numbers
+ * @a: first digit
+ * @b: second digit
+ * Return: always 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}#nclude "main.h"
+/**
+ * mul - multiply two numbers
+ * @a: first digit
+ * @b: second digit
+ * Return: always 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}#nclude "main.h"
+/**
+ * mul - multiply two numbers
+ * @a: first digit
+ * @b: second digit
+ * Return: always 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}#nclude "main.h"
+/**
+ * mul - multiply two numbers
+ * @a: first digit
+ * @b: second digit
+ * Return: always 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}#nclude "main.h"
+/**
+ * mul - multiply two numbers
+ * @a: first digit
+ * @b: second digit
+ * Return: always 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}#nclude "main.h"
+/**
+ * mul - multiply two numbers
+ * @a: first digit
+ * @b: second digit
+ * Return: always 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
