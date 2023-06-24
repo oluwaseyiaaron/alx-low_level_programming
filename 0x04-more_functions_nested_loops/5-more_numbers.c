62cc6960ce8416cb04121c33db4f6ff318ac461a
Aaron <whizzyrecx@gmail.com>
2023-06-23 17:41:26 -0700

    5-more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,7 +12,9 @@ void more_numbers(void)
 		j = 0;
 		while (j <= 14)
 		{
-			_putchar(j + '0');
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
 			j++;
 		}
 		_putchar('\n');
