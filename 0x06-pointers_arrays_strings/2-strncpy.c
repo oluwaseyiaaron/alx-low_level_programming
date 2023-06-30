c9e4d289a35e14645bf0eed288190bdb67dc5cbe
Aaron <whizzyrecx@gmail.com>
2023-06-30 04:51:09 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,23 +8,16 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	/**intialise a variable I*/
 	int i;
 
 	i = 0;
-	/*while i is less than the specified byte*/
-	/*AND the src is has not reach the null terminator*/
 	while (i < n && src[i] != '\0')
 	{
-		/*let the specified amount of the string
-		 * from src be equal to the dest variale*/
 		dest[i] = src[i];
 		i++;
 	}
-	/* but if i is still less than the specified n byte after s[i] = '\0'*/
 	while (i < n)
 	{
-		/*fill in the remaining n bytes with the '\0'*/
 		dest[i] = '\0';
 		i++;
 	}
