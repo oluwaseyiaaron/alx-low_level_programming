583c5c548f72252e56c6ad8f40beaa316e7a2d0c
Oluwaseyi Aaron <whizzyrecx@gmail.com>
2023-08-01 18:03:03 -0700

    print a list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,7 +3,23 @@
 
 /* standard C library */
 #include <stdio.h>
-
+#include <string.h>
+#include <stdlib.h>
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for ALX  project
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
 /* function prototype */
 size_t print_list(const list_t *h);
