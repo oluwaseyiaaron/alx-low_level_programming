5960c27b5cba50f276d933b82f2f18a1f43639c0
Oluwaseyi Aaron <whizzyrecx@gmail.com>
2023-08-01 18:13:36 -0700

    print element of list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,7 +6,7 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t n = 0;
+	size_t s = 0;
 
 	while (h)
 	{
@@ -17,8 +17,8 @@ size_t print_list(const list_t *h)
 		else
 			print("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		n++;
+		s++;
 	}
-	return (n);
+	return (s);
 }
 
